0dc40eebe9e195876e1cdd8f9b2a3ca0f7d87a06
Moises Alvarado <moises.alvarado.sme@outlook.com>
2017-03-27 16:08:32 -0400

    [Minicurso React con Firebase 2/4]: Cómo integrar Firebase en tu web y añadir autenticación

--- src/App.js
+++ src/App.js
@@ -1,18 +1,75 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase';
 
 class App extends Component {
+  constructor () {
+      super();
+      this.state = {
+        user: null
+      };
+      //Se Setea el contexto this a las funciones ajenas a React,
+      //es decir, las funciones que nosotros creamos.
+      this.handleAuth = this.handleAuth.bind(this);
+      this.handleLogOut = this.handleLogOut.bind(this);
+  }
+
+  componentWillMount() {
+    console.log("Aquí voy.");
+    firebase.auth().onAuthStateChanged(user => {
+      /*
+        ECMASCRIPT6 indica que si la clave y el valor son iguales
+        entonces se puede dejar la clave y automáticamente se interpreta
+        como <key>: <value>
+      */
+      this.setState({ user });
+      console.log(this.state);
+    });
+  }
+
+  handleAuth () {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+      .then(result => console.log(`${result.user.email} ha iniciado sesion`))
+      .catch(error => console.log(`${error.code}: ${error.message}`));
+  }
+
+  handleLogOut () {
+    firebase.auth().signOut()
+      .then(result => console.log(`${result.user.email} ha cerrado sesión.`))
+      .catch(error => console.log(`${error.code}: ${error.message}`));
+  }
+
+  renderLoginButton () {
+      //si el usuario está loggeado, entonces no mostramos el botón.:
+      if (this.state.user) {
+        return (
+          <div>
+            <img width="200" src={this.state.user.photoURL} alt={this.state.user.displayName} />
+            <p> Hola, {this.state.user.displayName} aka m0ises2. </p>
+            <button onClick={this.handleLogOut}> LogOut </button>
+          </div>
+        );
+      } else {
+        //Sino lo está:
+        return (
+          <button onClick={this.handleAuth}>Login con Google</button>
+        );
+      }
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to m0ises2 PseudoGram</h2>
         </div>
-        <p className="App-intro">
-          To get started, EDIT <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-intro">
+          {this.renderLoginButton()}
+        </div>
+        <br/>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import firebase from 'firebase';
+
+firebase.initializeApp({
+    apiKey: "AIzaSyB5xW69DPd4jALgTx31sSxjWEfUmz3b9xI",
+    authDomain: "pseudogram-d77a9.firebaseapp.com",
+    databaseURL: "https://pseudogram-d77a9.firebaseio.com",
+    storageBucket: "pseudogram-d77a9.appspot.com",
+    messagingSenderId: "186660002424"
+});
 
 ReactDOM.render(
   <App />,
